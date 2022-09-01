c8a8eea6432049066d0c6b003a1880147df3e5af
Om Borda <omborda2002@gmail.com>
2022-09-02 00:42:21 +0530

    add

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../../components/Layout";
+// import Layout from "../../components/Layout";
 import DashboardLanding from "../../components/dashboardContent/dashboardLanding";
 function dashboard() {
   return (
